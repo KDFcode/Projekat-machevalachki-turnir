9bbac4cc2162705a185b01bced7e7ba5c0ccc49b
KDFcode <aleksandarristic93@gmail.com>
2023-03-07 10:57:55 +0100

    Add project files.

---
+++ Projekat mačevalački turnir/Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿
+namespace Projekat_mačevalački_turnir
+{
+    partial class Prijava
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnPotvrdi = new System.Windows.Forms.Button();
+            this.btnOtkazi = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbImePrez = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbImeKluba = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbDrzava = new System.Windows.Forms.TextBox();
+            this.gbDisciplina = new System.Windows.Forms.GroupBox();
+            this.rbRapir = new System.Windows.Forms.RadioButton();
+            this.rbSidesword = new System.Windows.Forms.RadioButton();
+            this.rbLongsword = new System.Windows.Forms.RadioButton();
+            this.gbDodaci = new System.Windows.Forms.GroupBox();
+            this.cbSmestaj = new System.Windows.Forms.CheckBox();
+            this.cbMajica = new System.Windows.Forms.CheckBox();
+            this.cbObrok = new System.Windows.Forms.CheckBox();
+            this.gbUloga = new System.Windows.Forms.GroupBox();
+            this.rbSudija = new System.Windows.Forms.RadioButton();
+            this.rbTakmi = new System.Windows.Forms.RadioButton();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.gbDisciplina.SuspendLayout();
+            this.gbDodaci.SuspendLayout();
+            this.gbUloga.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnPotvrdi
+            // 
+            this.btnPotvrdi.Location = new System.Drawing.Point(658, 54);
+            this.btnPotvrdi.Name = "btnPotvrdi";
+            this.btnPotvrdi.Size = new System.Drawing.Size(99, 69);
+            this.btnPotvrdi.TabIndex = 0;
+            this.btnPotvrdi.Text = "Potvrdi unos podataka";
+            this.btnPotvrdi.UseVisualStyleBackColor = true;
+            // 
+            // btnOtkazi
+            // 
+            this.btnOtkazi.Location = new System.Drawing.Point(658, 171);
+            this.btnOtkazi.Name = "btnOtkazi";
+            this.btnOtkazi.Size = new System.Drawing.Size(99, 69);
+            this.btnOtkazi.TabIndex = 1;
+            this.btnOtkazi.Text = "Otkaži unos podataka";
+            this.btnOtkazi.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ime i prezime";
+            // 
+            // tbImePrez
+            // 
+            this.tbImePrez.Location = new System.Drawing.Point(15, 39);
+            this.tbImePrez.Name = "tbImePrez";
+            this.tbImePrez.Size = new System.Drawing.Size(185, 22);
+            this.tbImePrez.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ime kluba";
+            // 
+            // tbImeKluba
+            // 
+            this.tbImeKluba.Location = new System.Drawing.Point(18, 114);
+            this.tbImeKluba.Name = "tbImeKluba";
+            this.tbImeKluba.Size = new System.Drawing.Size(182, 22);
+            this.tbImeKluba.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Šifra države";
+            // 
+            // tbDrzava
+            // 
+            this.tbDrzava.Location = new System.Drawing.Point(18, 193);
+            this.tbDrzava.Name = "tbDrzava";
+            this.tbDrzava.Size = new System.Drawing.Size(182, 22);
+            this.tbDrzava.TabIndex = 7;
+            // 
+            // gbDisciplina
+            // 
+            this.gbDisciplina.Controls.Add(this.rbRapir);
+            this.gbDisciplina.Controls.Add(this.rbSidesword);
+            this.gbDisciplina.Controls.Add(this.rbLongsword);
+            this.gbDisciplina.Location = new System.Drawing.Point(21, 288);
+            this.gbDisciplina.Name = "gbDisciplina";
+            this.gbDisciplina.Size = new System.Drawing.Size(200, 117);
+            this.gbDisciplina.TabIndex = 8;
+            this.gbDisciplina.TabStop = false;
+            this.gbDisciplina.Text = "Odaberite disciplinu";
+            // 
+            // rbRapir
+            // 
+            this.rbRapir.AutoSize = true;
+            this.rbRapir.Location = new System.Drawing.Point(6, 79);
+            this.rbRapir.Name = "rbRapir";
+            this.rbRapir.Size = new System.Drawing.Size(61, 20);
+            this.rbRapir.TabIndex = 2;
+            this.rbRapir.TabStop = true;
+            this.rbRapir.Text = "Rapir";
+            this.rbRapir.UseVisualStyleBackColor = true;
+            // 
+            // rbSidesword
+            // 
+            this.rbSidesword.AutoSize = true;
+            this.rbSidesword.Location = new System.Drawing.Point(6, 49);
+            this.rbSidesword.Name = "rbSidesword";
+            this.rbSidesword.Size = new System.Drawing.Size(92, 20);
+            this.rbSidesword.TabIndex = 1;
+            this.rbSidesword.TabStop = true;
+            this.rbSidesword.Text = "Sidesword";
+            this.rbSidesword.UseVisualStyleBackColor = true;
+            // 
+            // rbLongsword
+            // 
+            this.rbLongsword.AutoSize = true;
+            this.rbLongsword.Location = new System.Drawing.Point(7, 22);
+            this.rbLongsword.Name = "rbLongsword";
+            this.rbLongsword.Size = new System.Drawing.Size(94, 20);
+            this.rbLongsword.TabIndex = 0;
+            this.rbLongsword.TabStop = true;
+            this.rbLongsword.Text = "Longsword";
+            this.rbLongsword.UseVisualStyleBackColor = true;
+            // 
+            // gbDodaci
+            // 
+            this.gbDodaci.Controls.Add(this.cbSmestaj);
+            this.gbDodaci.Controls.Add(this.cbMajica);
+            this.gbDodaci.Controls.Add(this.cbObrok);
+            this.gbDodaci.Location = new System.Drawing.Point(456, 288);
+            this.gbDodaci.Name = "gbDodaci";
+            this.gbDodaci.Size = new System.Drawing.Size(200, 117);
+            this.gbDodaci.TabIndex = 9;
+            this.gbDodaci.TabStop = false;
+            this.gbDodaci.Text = "Odaberite opcione dodatke";
+            // 
+            // cbSmestaj
+            // 
+            this.cbSmestaj.AutoSize = true;
+            this.cbSmestaj.Location = new System.Drawing.Point(7, 79);
+            this.cbSmestaj.Name = "cbSmestaj";
+            this.cbSmestaj.Size = new System.Drawing.Size(78, 20);
+            this.cbSmestaj.TabIndex = 2;
+            this.cbSmestaj.Text = "Smeštaj";
+            this.cbSmestaj.UseVisualStyleBackColor = true;
+            // 
+            // cbMajica
+            // 
+            this.cbMajica.AutoSize = true;
+            this.cbMajica.Location = new System.Drawing.Point(7, 49);
+            this.cbMajica.Name = "cbMajica";
+            this.cbMajica.Size = new System.Drawing.Size(69, 20);
+            this.cbMajica.TabIndex = 1;
+            this.cbMajica.Text = "Majica";
+            this.cbMajica.UseVisualStyleBackColor = true;
+            // 
+            // cbObrok
+            // 
+            this.cbObrok.AutoSize = true;
+            this.cbObrok.Location = new System.Drawing.Point(7, 22);
+            this.cbObrok.Name = "cbObrok";
+            this.cbObrok.Size = new System.Drawing.Size(66, 20);
+            this.cbObrok.TabIndex = 0;
+            this.cbObrok.Text = "Obrok";
+            this.cbObrok.UseVisualStyleBackColor = true;
+            // 
+            // gbUloga
+            // 
+            this.gbUloga.Controls.Add(this.rbSudija);
+            this.gbUloga.Controls.Add(this.rbTakmi);
+            this.gbUloga.Location = new System.Drawing.Point(240, 288);
+            this.gbUloga.Name = "gbUloga";
+            this.gbUloga.Size = new System.Drawing.Size(200, 117);
+            this.gbUloga.TabIndex = 10;
+            this.gbUloga.TabStop = false;
+            this.gbUloga.Text = "Odaberite ulogu na turniru";
+            // 
+            // rbSudija
+            // 
+            this.rbSudija.AutoSize = true;
+            this.rbSudija.Location = new System.Drawing.Point(7, 79);
+            this.rbSudija.Name = "rbSudija";
+            this.rbSudija.Size = new System.Drawing.Size(66, 20);
+            this.rbSudija.TabIndex = 1;
+            this.rbSudija.TabStop = true;
+            this.rbSudija.Text = "Sudija";
+            this.rbSudija.UseVisualStyleBackColor = true;
+            // 
+            // rbTakmi
+            // 
+            this.rbTakmi.AutoSize = true;
+            this.rbTakmi.Location = new System.Drawing.Point(7, 22);
+            this.rbTakmi.Name = "rbTakmi";
+            this.rbTakmi.Size = new System.Drawing.Size(104, 20);
+            this.rbTakmi.TabIndex = 0;
+            this.rbTakmi.TabStop = true;
+            this.rbTakmi.Text = "Takmičar/ka";
+            this.rbTakmi.UseVisualStyleBackColor = true;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Prijava
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gbUloga);
+            this.Controls.Add(this.gbDodaci);
+            this.Controls.Add(this.gbDisciplina);
+            this.Controls.Add(this.tbDrzava);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbImeKluba);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbImePrez);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnOtkazi);
+            this.Controls.Add(this.btnPotvrdi);
+            this.Name = "Prijava";
+            this.Text = "Prijava za turnir u mačevanju";
+            this.Load += new System.EventHandler(this.Prijava_Load);
+            this.gbDisciplina.ResumeLayout(false);
+            this.gbDisciplina.PerformLayout();
+            this.gbDodaci.ResumeLayout(false);
+            this.gbDodaci.PerformLayout();
+            this.gbUloga.ResumeLayout(false);
+            this.gbUloga.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPotvrdi;
+        private System.Windows.Forms.Button btnOtkazi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbImePrez;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbImeKluba;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbDrzava;
+        private System.Windows.Forms.GroupBox gbDisciplina;
+        private System.Windows.Forms.RadioButton rbRapir;
+        private System.Windows.Forms.RadioButton rbSidesword;
+        private System.Windows.Forms.RadioButton rbLongsword;
+        private System.Windows.Forms.GroupBox gbDodaci;
+        private System.Windows.Forms.CheckBox cbSmestaj;
+        private System.Windows.Forms.CheckBox cbMajica;
+        private System.Windows.Forms.CheckBox cbObrok;
+        private System.Windows.Forms.GroupBox gbUloga;
+        private System.Windows.Forms.RadioButton rbSudija;
+        private System.Windows.Forms.RadioButton rbTakmi;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ Projekat mačevalački turnir/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projekat_mačevalački_turnir
+{
+    public partial class Prijava : Form
+    {
+        public Prijava()
+        {
+            InitializeComponent();
+        }
+
+        private void Prijava_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //nemoj ovako svakako
+        //prodji sve radiobuttone u jednom,pa drugom groupbox-u
+        //pa checbkox po istom onom foreach principu
+        //izbaci error ako nije sve pri ,,potvrdi''
+        // onaj ,,pomocnik;; ako predje preko necega da ti kaze sta treba i kako
+        //otkazi sve brise,fokus nazad na prvo polje
+        //povezi s bazom
+        //pri potvrdi unos svega u bazu,od teksta do box-ova
+        //dodaj nesto za pregled toga kao ovo s kraja liste strana
+        // zbirka
+        // str 35 primer koristi
+        //str 46&co primer koristi
+        //str 76 takodje
+        //str 84
+        //str 101
+        //str 123
+
+        /*
+        i ispitni zadaci
+                private void rbLongsword_CheckedChanged(object sender, EventArgs e)
+                {
+                    if rbLongsword; ///zasto ga ne prepoznaje?
+
+                }
+        */
+    }
+}
