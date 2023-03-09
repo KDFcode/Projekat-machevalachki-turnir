c607bfb353d57e6c4ee09f20b22350a79f1e2f61
KDFcode <113381213+KDFcode@users.noreply.github.com>
2023-03-09 15:44:23 +0100

    Update Form1.cs

--- Projekat mačevalački turnir/Form1.cs
+++ Projekat mačevalački turnir/Form1.cs
@@ -22,30 +22,5 @@ namespace Projekat_mačevalački_turnir
 
         }
 
-        //nemoj ovako svakako
-        //prodji sve radiobuttone u jednom,pa drugom groupbox-u
-        //pa checbkox po istom onom foreach principu
-        //izbaci error ako nije sve pri ,,potvrdi''
-        // onaj ,,pomocnik;; ako predje preko necega da ti kaze sta treba i kako
-        //otkazi sve brise,fokus nazad na prvo polje
-        //povezi s bazom
-        //pri potvrdi unos svega u bazu,od teksta do box-ova
-        //dodaj nesto za pregled toga kao ovo s kraja liste strana
-        // zbirka
-        // str 35 primer koristi
-        //str 46&co primer koristi
-        //str 76 takodje
-        //str 84
-        //str 101
-        //str 123
-
-        /*
-        i ispitni zadaci
-                private void rbLongsword_CheckedChanged(object sender, EventArgs e)
-                {
-                    if rbLongsword; ///zasto ga ne prepoznaje?
-
-                }
-        */
     }
 }
